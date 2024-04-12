36453b2952ddab3e61c00b5434897ed633eb66b2
Dean James <st10378305@vcconnect.edu.za>
2024-04-12 16:31:50 +0200

    Added a Recipe class
    I created a class called Recipe, which will consist of its attributes relating to the recipe requirements such as, the ingredients, the steps, the name of the recipe, and the factor that the recipe will be scaled by.
    
    I added some functions with no functionality to help brainstorm, the flow of logic for my code.

---
+++ PROG6221 POE/Recipe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_POE
+{
+    class Recipe
+    {
+        public int scaleFactor { get; set; }
+        public string name { get; set; }
+        public string[] steps { get; set; }
+        public string[,] ingredients { get; set; }
+
+        public Recipe() { 
+        }
+
+        public Recipe(string _name, string[] _steps, string[,] _ingredients)
+        {
+            name = _name;
+            steps = _steps;
+            ingredients = _ingredients;
+        }
+
+        public string Scale()
+        {
+            return "";
+        }
+
+        public string DisplayRecipe()
+        {
+            return "";
+        }
+
+        public void IngredientPrompt()
+        {
+
+        }
+
+        public void StepsPrompt()
+        {
+
+        }
+
+    }
+}
