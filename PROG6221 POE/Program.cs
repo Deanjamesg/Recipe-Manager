811a427ffc19c659e70c59cd1b5e24526ccbd07d
Dean James <st10378305@vcconnect.edu.za>
2024-04-14 21:03:06 +0200

    Added functionality to DisplayRecipe() and CreateNewRecipe()
    In the Program class, I created a test sample, to ensure that my DisplayRecipe() function works. With the use of a pre-set sample, it just makes for efficient work as I don't need to manually enter a recipe in to see if I am getting the desired output.
    
    I also added functionality to CreateNewRecipe(), which takes in user input, and stores it in the memory of the object.

--- PROG6221 POE/Program.cs
+++ PROG6221 POE/Program.cs
@@ -15,9 +15,28 @@ namespace PROG6221_POE
 
             Boolean application;
 
-            Recipe recipe;
+            Recipe recipe = new Recipe
+            {
+            name = "Bacon Pasta",
+            scaleFactor = 1,
+            ingredients = new string[,]
+            {
+                { "Basil", "1", "cup" },
+                { "Bacon", "200", "grams" },
+                { "Whole peeled tomatoes", "400", "grams" },
+                { "Penne", "250", "grams" }
+            },
+            steps = new string[]
+            {
+                "Cook the bacon, in a deep seated pan, until crispy (to your preference).",
+                "Add the whole peeled tomatoes to the pan. Add the cup of basil to the tomato sauce.",
+                "Bring to a boil and then let it simmer for 20 - 30 minutes.",
+                "Cook the pasta whilst the dish is simmering.",
+                "Once the sauce has finished cooking, as well as the pasta, put the sauce in the pasta pot and mix it.",
+                "Food is ready to eat, can be served with fresh basil on top."
+            }
+            };
 
-            recipe = new Recipe();
             application = true;
 
             Console.Title = "RECIPE APPLICATION";
@@ -27,14 +46,15 @@ namespace PROG6221_POE
 
             while (application)
             {
-                Console.WriteLine("Please select one of the following options: \n\n1) Add a new recipe \n2) Scale a recipe \n3) Reset scale to original \n4) Display a recipe \n5) Delete a recipe \n6) Exit");
+                Console.WriteLine("Please select one of the following options: \n\n1) Create a new recipe \n2) Scale a recipe \n3) Reset scale to original \n4) Display recipe \n5) Delete recipe \n6) Exit");
                 option = Console.ReadLine();
 
                 switch (option)
                 {
-                    case "1": //Add a New Recipe
+                    case "1": //Create a New Recipe
 
-                        recipe.AddNewRecipe();
+                        recipe = new Recipe();
+                        recipe.CreateNewRecipe();
                         break;
 
                     case "2": //Scale a Recipe
@@ -64,7 +84,7 @@ namespace PROG6221_POE
 
                     default:
                         Console.WriteLine("Incorrect value was entered.");
-                        Console.WriteLine("\nEnter any key to continue.");
+                        Console.WriteLine("\nPress any key to continue...");
                         Console.ReadKey();
                         Console.Clear();
                         break;
--- PROG6221 POE/Recipe.cs
+++ PROG6221 POE/Recipe.cs
@@ -16,14 +16,6 @@ namespace PROG6221_POE
         public Recipe() { 
         }
 
-        public Recipe(string recipeName, string[] recipeSteps, string[,] recipeIngredients)
-        {
-            scaleFactor = 1;
-            name = recipeName;
-            steps = recipeSteps;
-            ingredients = recipeIngredients;
-        }
-
         public void ScaleRecipe()
         {
 
@@ -31,11 +23,34 @@ namespace PROG6221_POE
 
         public void ResetScale()
         {
+            scaleFactor = 1;
+            Console.Clear();
+            DisplayRecipe();
 
         }
 
         public void DisplayRecipe()
         {
+
+            Console.Clear();
+            Console.WriteLine(name);
+            Console.WriteLine("-----------------------------------\nIngredients: \n-----------------------------------");
+
+            for (int i = 0; i < ingredients.GetLength(0); i++)
+            {
+                Console.WriteLine(ingredients[i, 1] + " " + ingredients[i, 2] + " of " + ingredients[i, 0]);
+
+            }
+            Console.WriteLine("-----------------------------------\nSteps: \n-----------------------------------");
+
+            for (int i = 0; i < steps.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ") " + steps[i]);
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
 
         }
 
@@ -44,18 +59,48 @@ namespace PROG6221_POE
 
         }
 
-        public void AddNewRecipe()
+        public void CreateNewRecipe()
         {
+            Console.Clear();
+            Console.WriteLine("Please enter the following requirements: ");
 
-        }
+            Console.Write("\nName of the recipe: ");
+            name = Console.ReadLine();
 
-        public void IngredientPrompt()
-        { 
+            Console.Write("\nNumber of ingredients required: ");
+            int ingredientsCount = int.Parse(Console.ReadLine());
 
-        }
+            Console.Write("\nNumber of steps to be done: ");
+            int stepsCount = int.Parse(Console.ReadLine());
 
-        public void StepsPrompt()
-        {
+            scaleFactor = 1;
+            ingredients = new string[ingredientsCount, 3];
+            steps = new string[stepsCount];
+
+            string[] Prompt = { "Ingredient: ", "Quantity: ", "Unit of measurement: " };
+
+            Console.WriteLine("\nPlease enter the ingredients used, the quantity and unit of measurement: ");
+
+            for (int i = 0; i < ingredients.GetLength(0); i++)
+            {
+                Console.WriteLine("\nINGREDIENT NUMBER: " + (i + 1));
+
+                for (int j = 0; j < ingredients.GetLength(1); j++)
+                {
+                    Console.Write(Prompt[j]);
+                    ingredients[i, j] = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("\nPlease enter a description for each step: ");
+
+            for (int i = 0; i < steps.Length; i++)
+            {
+                Console.WriteLine("\nSTEP NUMBER: " + (i + 1));
+                steps[i] = Console.ReadLine();
+            }
+            Console.Clear();
+
+            DisplayRecipe();
 
         }
 
