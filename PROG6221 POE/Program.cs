eccef6d397b9645b7eacbcb2468214b7b8358200
Dean James <st10378305@vcconnect.edu.za>
2024-04-14 17:29:29 +0200

    Added a user prompt
    I added a user prompt to the Program class.
    
    Based off of the user's input, their input will be passed through a switch statement, and from there the methods will be called, accordingly.

--- PROG6221 POE/Program.cs
+++ PROG6221 POE/Program.cs
@@ -10,7 +10,68 @@ namespace PROG6221_POE
     {
         public static void Main(string[] args)
         {
-            
+
+            int ingredientCount, stepCount, index, scale;
+
+            string option, confirm;
+
+            Boolean application;
+
+            Recipe recipe;
+
+            recipe = new Recipe();
+            application = true;
+
+            Console.Title = "RECIPE APPLICATION";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("Welcome to the Recipe Application!\n");
+
+            while (application)
+            {
+                Console.WriteLine("Please select one of the following options: \n\n1) Add a new recipe \n2) Scale a recipe \n3) Display a recipe \n4) Show all saved recipes \n5) Delete a recipe \n6) Exit");
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1": //Add a New Recipe
+
+                        recipe.AddNewRecipe();
+                        break;
+
+                    case "2": //Scale a Recipe
+
+                        recipe.ScaleRecipe();
+                        break;
+
+                    case "3": //Display a Recipe
+
+                        recipe.DisplayRecipe();
+                        break;
+
+                    case "4": //Display all Recipes
+
+                        recipe.DisplayRecipe(); //Pass all recipes in list
+                        break;
+
+                    case "5": //Delete Recipe
+
+                        recipe.DeleteRecipe();
+                        break;
+
+                    case "6": //Exit Application
+                        application = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Incorrect value was entered.");
+                        Console.WriteLine("\nEnter any key to continue.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+
         }
     }
 }
